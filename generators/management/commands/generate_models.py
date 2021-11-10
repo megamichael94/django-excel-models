973443d3f9ade424dbe931c0b7d790583fb74941
michael mestas <michaelmestaspaz@gmail.com>
2021-11-09 22:54:56 -0500

    #1 initial commit, app creation

---
+++ generators/management/commands/generate_models.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from polls.models import Question as Poll
+
+
+class Command(BaseCommand):
+    """
+    Class that handles the command itself, parses the excel file and creates a
+    model to later handle the
+    """
+
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        #todo: check nargs params
+        parser.add_argument('file_path', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        """
+        Function called once the command has been called.
+        #todo: finish docstring and add to help
+        """
+                
+        self.stdout.write(
+            self.style.SUCCESS('Successfully imported excel files'))
